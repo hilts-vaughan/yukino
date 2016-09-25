318501df3ad711d768f70f57aa1ce97fc0776b2b
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-09-25 14:18:15 -0400

    wip 2

--- src/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * yukino - Yukino is a music search crawler that is capable of downloading, identifying (with Gracenote) and serving up music from various web services.
- * It excels at making music accessible to all, using just Youtube and SndCloud
- */
-while (true) {
-    console.log('hello world...');
-    console.log('Something something desu %s', 12);
-}
